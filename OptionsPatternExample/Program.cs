be3a428915416732e2e17caec22b885bb6637f4c
İlhan Enes Daniş <danisenes3@gmail.com>
2025-03-31 13:00:17 +0300

    configure options pattern

--- OptionsPatternExample/Program.cs
+++ OptionsPatternExample/Program.cs
@@ -1,12 +1,16 @@
+using Microsoft.Extensions.Options;
+using OptionsPatternExample.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+
+builder.Services.Configure<ApplicationOptions>(builder.Configuration.GetSection(nameof(ApplicationOptions)));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +18,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("options", (IOptions<ApplicationOptions> options,
+                       IOptionsSnapshot<ApplicationOptions> optionsSnapshot,
+                       IOptionsMonitor<ApplicationOptions> optionsMonitor) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var response = new
+    {
+        OptionsValue = options.Value.ExampleValue,
+        SnapshotValue = optionsSnapshot.Value.ExampleValue,
+        MonitorValue = optionsMonitor.CurrentValue.ExampleValue
+    };
+
+    return Results.Ok(response);
+});
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
